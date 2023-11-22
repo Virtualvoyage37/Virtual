0073db9832cbfe75c0e6241111ddaa37c9154650
Virtualvoyage37 <virtualvoyage37@gmail.com>
2023-11-22 14:05:21 +0530

    new auth files

---
+++ src/app/auth/firebaseAuth.ts
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: 'AIzaSyBG_oRjMAUEgJ8ZNzMMdU7bHUMZ2UV2keQ',
+  authDomain: 'virtual-37.firebaseapp.com',
+  projectId: 'virtual-37',
+  storageBucket: 'virtual-37.appspot.com',
+  messagingSenderId: '235494079700',
+  appId: '1:235494079700:web:2111f4e07b8e06c55ccf41',
+  measurementId: 'G-KMYLVWWK7Y',
+};
